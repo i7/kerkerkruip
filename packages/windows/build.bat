7f8b2a7ca588fb47e0f356e4912ca3d79c080341
Dannii <curiousdannii@gmail.com>
2013-02-21 20:19:50 +1000

    Add a UI to the Windows installer (#14)

--- packages/windows/build.bat
+++ packages/windows/build.bat
@@ -2,5 +2,5 @@
 setlocal
 set PATH=%PATH%;"C:\Program Files (x86)\WiX Toolset v3.7\bin";"C:\Program Files\WiX Toolset v3.7\bin"
 candle kerkerkruip.wxs
-light kerkerkruip.wixobj -out kerkerkruip-7.msi
+light -ext WixUIExtension -cultures:en-us kerkerkruip.wixobj
 @pause
