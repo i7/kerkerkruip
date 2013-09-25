i7/kerkerkruip

commit 807f02319329786336e279e370ddf6ae9c1aaa1e

Menus now use a popover window to avoid erasing the main window (#114)

Projects/Kerkerkruip.inform/Source/story.ni
@@ -5,17 +5,6 @@ The story genre is "dungeon crawl".
 The release number is 9.
 Release along with [cover art and] a file of "Gargoyle config file" called "Kerkerkruip.ini".
 
-Section - 3rd Party Inclusions
-	
-Include version 1/130922 of Menus by Dannii Willis. [Get this at https://github.com/i7/extensions]
-Include Basic Screen Effects by Emily Short. 
-Include Numbered Disambiguation Choices by Aaron Reed.
-Include Glulx Entry Points by Emily Short.
-Include version 7/130712 of Dynamic Objects by Jesse McGrew. [Get this at https://github.com/i7/extensions]
-Include Glulx Status Window Control by Erik Temple.
-Include Fixed Point Maths by Michael Callaghan.
-Include Questions by Michael Callaghan.
-
 Section - Glimmr or Flexible Windows
 
 [Testing with Glimmr installed will be slower, due to compilation and graphics, so commits to story.ni would generally best be made with it commented out. Be sure to comment in Flexible Windows instead!]
@@ -26,6 +15,19 @@ Include Flexible Windows by Jon Ingold.
 
 Include Kerkerkruip Windows by Erik Temple.
 [Include Extended Debugging by Erik Temple.]
+
+Section - 3rd Party Inclusions
+	
+Include version 1/130923 of Menus by Dannii Willis. [Get this at https://github.com/i7/extensions]
+Include Basic Screen Effects by Emily Short. 
+Include Numbered Disambiguation Choices by Aaron Reed.
+Include Glulx Entry Points by Emily Short.
+Include version 7/130712 of Dynamic Objects by Jesse McGrew. [Get this at https://github.com/i7/extensions]
+Include Glulx Status Window Control by Erik Temple.
+Include Fixed Point Maths by Michael Callaghan.
+Include Questions by Michael Callaghan.
+
+
 
 
 Section - Include Permadeath
@@ -115,11 +117,6 @@ Section - Relist rock validation rule (not for release)
 
 The rock validation rule is not listed in the when play begins rules. The rock validation rule is listed before the show the title screen rule in the startup rules.
 
-
-Section - Menu code adjustments
-
-Before displaying:
-	open the status window.
 
 
 Section - Defining perform syntax (not for use with Glimmr Canvas Animation by Erik Temple)

Extensions/Erik Temple/Kerkerkruip Windows.i7x
@@ -389,13 +389,23 @@ This is the open up game windows rule:
 	open side windows;
 	open the status window.
 
+
+
+Section - Menus & windows
+
 [We need to close and open the side windows when we visit the in-game help menus. "In-game menu checking" is a kind of action that identifies the way we get to the menus while playing the game.]
-Before displaying when in-game menu-checking:
-	close side windows.
-	
-After displaying when in-game menu-checking:
+
+Before displaying:
+	close side windows;
+	open the status window;
+
+Last after displaying:
 	close the status window;
-	consider the open up game windows rule.
+	if in-game menu-checking:
+		open side windows;
+		open the status window;
+
+
 
 [We also need to reset the windows when a game is restored.]
 After restoring from a saved game:

Extensions/Victor Gijsbers/Kerkerkruip Actions and UI.i7x
@@ -825,6 +825,8 @@ A pickpocket rule (this is the readied weapons pickpocket rule):
 
 
 
+
+
 Chapter - Options menu
 
 Optionsmenuing is an action out of world. Optionsmenuing is in-game menu-checking.
@@ -833,10 +835,7 @@ Understand "options" and "settings" as Optionsmenuing.
 
 Carry out optionsmenuing:
 	display the (Table of Options Menu) menu with title "Options";
-	try looking;
-	report the main action again;
-
-
+	say "Ok.";
 
 Chapter - Achievements Menu
 
@@ -846,9 +845,7 @@ Understand "achieve" and "achievements" as achievemenuing.
 
 Carry out achievemenuing:
 	display the (Table of Achievement Menu) menu with title "Achievements";
-	try looking;
-	report the main action again;
-
+	say "Ok.";
 
 Chapter - Enemies and Powers Menu
 
@@ -858,9 +855,7 @@ Understand "enemies" and "powers" as powermenuing.
 
 Carry out powermenuing:
 	display the Table of Power Help menu with title "Enemies and powers";
-	try looking;
-	report the main action again;
-
+	say "Ok.";
 
 Chapter - The asking for help action (for use without Basic Help Menu by Emily Short)
 
@@ -875,9 +870,8 @@ Understand "menu" as asking for help.
 The help request rule is not listed in any rulebook.
 
 Carry out asking for help:
-	display the Table of Kerkerkruip Help menu with title "Help menu";
-	try looking;
-	report the main action again;
+	display the Table of Kerkerkruip Help menu with title "Kerkerkruip";
+	say "Ok.";
 
 Chapter - Credits menu
 
@@ -888,7 +882,11 @@ Requesting the story file version is in-game menu-checking.
 The announce the story file version rule is not listed in any rulebook.
 
 Carry out requesting the story file version:
-	show menu page "[variable letter spacing][total credits][paragraph break]" with title "Credits, Copyright & Afterword";
+	show menu page "[total credits]" with title "Credits, Copyright & Afterword";
+	say "Ok.";
+
+
+
 
 
 Chapter - Replacing "You can see" is not a joke

Extensions/Victor Gijsbers/Kerkerkruip Start and Finish.i7x
@@ -277,7 +277,7 @@ Menu command menu:
 	rule succeeds with result the show the menu rule;
 
 This is the show the menu rule:
-	display the Table of Kerkerkruip Help menu with title "Help menu";
+	display the Table of Kerkerkruip Help menu with title "Kerkerkruip";
 
 
 

commit 20bf7f0ae0b06a9b0001f666d520e31e17f2fdd0

Fixed ordering of extension include directives to fix issue introduced in 807f02319329786336e279e370ddf6ae9c1aaa1e

Projects/Kerkerkruip.inform/Source/story.ni
@@ -5,20 +5,8 @@ The story genre is "dungeon crawl".
 The release number is 9.
 Release along with [cover art and] a file of "Gargoyle config file" called "Kerkerkruip.ini".
 
-Section - Glimmr or Flexible Windows
-
-[Testing with Glimmr installed will be slower, due to compilation and graphics, so commits to story.ni would generally best be made with it commented out. Be sure to comment in Flexible Windows instead!]
-[Include Kerkerkruip Glimmr Additions by Erik Temple.]
-
-Include Flexible Windows by Jon Ingold.
-
-
-Include Kerkerkruip Windows by Erik Temple.
-[Include Extended Debugging by Erik Temple.]
-
 Section - 3rd Party Inclusions
 	
-Include version 1/130923 of Menus by Dannii Willis. [Get this at https://github.com/i7/extensions]
 Include Basic Screen Effects by Emily Short. 
 Include Numbered Disambiguation Choices by Aaron Reed.
 Include Glulx Entry Points by Emily Short.
@@ -27,7 +15,14 @@ Include Glulx Status Window Control by Erik Temple.
 Include Fixed Point Maths by Michael Callaghan.
 Include Questions by Michael Callaghan.
 
+Section - Graphics and Windows
 
+[Testing with Glimmr installed will be slower, due to compilation and graphics, so commits to story.ni would generally best be made with it commented out. It is not necessary to comment out Flexible Windows when Glimmr is commented in.]
+Include Flexible Windows by Jon Ingold.
+[Include Kerkerkruip Glimmr Additions by Erik Temple.]
+Include version 1/130922 of Menus by Dannii Willis. [Get this at https://github.com/i7/extensions]
+Include Kerkerkruip Windows by Erik Temple.
+[Include Extended Debugging by Erik Temple.]
 
 
 Section - Include Permadeath
@@ -117,6 +112,11 @@ Section - Relist rock validation rule (not for release)
 
 The rock validation rule is not listed in the when play begins rules. The rock validation rule is listed before the show the title screen rule in the startup rules.
 
+
+Section - Menu code adjustments
+
+Before displaying:
+	open the status window.
 
 
 Section - Defining perform syntax (not for use with Glimmr Canvas Animation by Erik Temple)